78720b68543e309a958174f29396b66b7fe25273
Guofan Xu <xuguofan@live.com>
2019-04-12 22:00:55 +0800

    add count and say (what a fuck description)

---
+++ src/main/kotlin/CountAndSay.kt
@@ -0,0 +1,30 @@
+fun countAndSay(n: Int): String {
+    var prev = arrayListOf('1')
+    for (i in 2..n) {
+        val array = ArrayList<Char>()
+        var c = prev[0]
+        var count = 1
+        for (j in 1 until prev.size) {
+            if (prev[j] != c) {
+                array.add((count + 48).toChar())
+                array.add(c)
+                c = prev[j]
+                count = 1
+            } else {
+                count++
+            }
+        }
+        array.add((count + 48).toChar())
+        array.add(c)
+        prev = array
+    }
+    return String(prev.toCharArray())
+}
+
+
+fun main() {
+    println(countAndSay(1))
+    println(countAndSay(2))
+    println(countAndSay(3))
+    println(countAndSay(4))
+}
