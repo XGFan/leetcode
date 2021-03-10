d0e8449c139c5d25d52727945252b2fbe9afe928
Guofan Xu <xuguofan@live.com>
2021-03-11 00:47:45 +0800

    improve Permutation Sequence little
    Signed-off-by: Guofan Xu <xuguofan@live.com>

--- go/PermutationSequence.go
+++ go/PermutationSequence.go
@@ -37,6 +37,43 @@ func next(nums []int) {
 	}
 }
 
+func getPermutation2(n int, k int) string {
+	ints := make([]int, n)
+	total := 1
+	t := 0
+	for i := 1; i <= n; i++ {
+		total *= i
+		if k <= total {
+			t = k - total/i
+			total = i - 1
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		if i < n-total {
+			ints[i] = i + 1
+		} else {
+			ints[n-1-(i-(n-total))] = i + 1
+		}
+	}
+	//fmt.Println(total, t, ints)
+	for i := 0; i < t; i++ {
+		next(ints)
+	}
+	builder := strings.Builder{}
+	for _, v := range ints {
+		builder.WriteByte(byte(v + 48))
+	}
+	return builder.String()
+}
 func main() {
-	fmt.Println(getPermutation(3, 3))
+	fmt.Println(getPermutation(3, 6))
+	fmt.Println(getPermutation2(3, 6))
+	fmt.Println(getPermutation(4, 13))
+	fmt.Println(getPermutation2(4, 13))
+	fmt.Println(getPermutation(9, 132))
+	fmt.Println(getPermutation2(9, 132))
+	fmt.Println(getPermutation(4, 1))
+	fmt.Println(getPermutation2(4, 1))
+
 }
