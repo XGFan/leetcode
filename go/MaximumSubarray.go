70cc52476f8d4efedea8aaee9c2dfa0a9b843165
Guofan Xu <xuguofan@live.com>
2021-03-10 15:29:31 +0800

    add Maximum Subarray
    Signed-off-by: Guofan Xu <xuguofan@live.com>

---
+++ go/MaximumSubarray.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func maxSubArray(nums []int) int {
+	sum := nums[0]
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		v := nums[i]
+		if v >= 0 {
+			if sum >= 0 {
+				sum += v
+			} else {
+				sum = v
+			}
+			max = maxV(max, sum)
+		} else {
+			if sum >= 0 {
+				sum += v
+				//no need to update max
+			} else {
+				sum = v
+				max = maxV(max, sum)
+			}
+		}
+	}
+	return max
+}
+
+func maxV(a, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func main() {
+	fmt.Println(maxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}))
+	fmt.Println(maxSubArray([]int{0}))
+	fmt.Println(maxSubArray([]int{-2, -1}))
+}
