e4aa1096aab21f3a6ee3fa2b4f999917b30c085e
Guofan Xu <xuguofan@live.com>
2021-03-10 23:02:25 +0800

    improve Insert Interval
    Signed-off-by: Guofan Xu <xuguofan@live.com>

--- go/InsertInterval.go
+++ go/InsertInterval.go
@@ -3,49 +3,47 @@ package main
 import "fmt"
 
 func insert(intervals [][]int, newInterval []int) [][]int {
-	f, t := newInterval[0], newInterval[1]
-	for j := 0; j < len(intervals); j++ {
+	l := len(intervals)
+	fi, ti := l, l
+	for j := 0; j < l; j++ {
+		if fi == l && ti != l {
+			break
+		}
 		ints := intervals[j]
-		if t < ints[0] {
-			//...f...t...o...o...
-			//insert and continue
-			intervals = append(intervals, newInterval) //增加空间
-			copy(intervals[j+1:], intervals[j:])       //挪动
-			intervals[j] = newInterval                 //赋值
-			return intervals
-		} else if t < ints[1] {
-			if f < ints[0] {
-				//...f...o...t...o...
-				ints[0] = f
-				return intervals
-			} else {
-				//...o...f...t...o...
-				return intervals
+		if fi == l {
+			if newInterval[0] <= ints[0] {
+				fi = j
+			} else if newInterval[0] <= ints[1] {
+				fi = j
+				newInterval[0] = ints[0]
 			}
-		} else {
-			if f <= ints[0] {
-				//...f...o...o...t...
-				ints[0] = f
-				ints[1] = t
-				copy(intervals[j:], intervals[j+1:])
-				return insert(intervals[:len(intervals)-1], ints)
-			} else if f <= ints[1] {
-				//...o...f...o...t...
-				ints[1] = t
-				copy(intervals[j:], intervals[j+1:])
-				return insert(intervals[:len(intervals)-1], ints)
-			} else {
-				//...o...o...x...x...
-				//下一轮处理
+		}
+		if ti == l {
+			if newInterval[1] < ints[0] {
+				ti = j
+			} else if newInterval[1] <= ints[1] {
+				ti = j + 1
+				newInterval[1] = ints[1]
 			}
 		}
 	}
-	return append(intervals, newInterval)
+	//fmt.Println(fi, ti, newInterval)
+	//fmt.Println(intervals[:fi], newInterval, intervals[ti:])
+	intervals = append(intervals, []int{})
+	copy(intervals[fi+1:], intervals[ti:])
+	intervals[fi] = newInterval
+	return intervals[:len(intervals)-ti+fi]
 }
 
 func main() {
 	fmt.Println(insert([][]int{}, []int{5, 7}), "=", [][]int{{5, 7}})
 	fmt.Println(insert([][]int{{1, 5}}, []int{2, 3}), "=", [][]int{{1, 5}})
 	fmt.Println(insert([][]int{{1, 5}}, []int{2, 7}), "=", [][]int{{1, 7}})
-
+	fmt.Println(insert([][]int{{1, 2}}, []int{2, 4}), "=", [][]int{{1, 4}})
+	fmt.Println(insert([][]int{{3, 2}}, []int{1, 5}), "=", [][]int{{1, 5}})
+	fmt.Println(insert([][]int{{3, 2}}, []int{1, 5}), "=", [][]int{{1, 5}})
+	fmt.Println(insert([][]int{{1, 3}, {6, 9}}, []int{2, 5}), "=", [][]int{{1, 5}, {6, 9}})
+	fmt.Println(insert([][]int{{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}}, []int{4, 8}), "=", [][]int{{1, 2}, {3, 10}, {12, 16}})
+	fmt.Println(insert([][]int{{1, 5}}, []int{0, 0}), "=", [][]int{{0, 0}, {1, 5}})
+	fmt.Println(insert([][]int{{3, 5}, {12, 15}}, []int{6, 6}), "=", [][]int{{3, 5}, {6, 6}, {12, 15}})
 }
