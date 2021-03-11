dea5719c58cc80f375b7a9a754d7951a929154e7
Guofan Xu <xuguofan@live.com>
2021-03-12 00:10:53 +0800

    improve Unique Paths II

--- go/UniquePathsII.go
+++ go/UniquePathsII.go
@@ -5,12 +5,8 @@ import "fmt"
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 	m := len(obstacleGrid)
 	n := len(obstacleGrid[0])
-	table := make([][]int, m)
-	for i := range table {
-		table[i] = make([]int, n)
-	}
 	if obstacleGrid[m-1][n-1] == 0 {
-		table[m-1][n-1] = 1
+		obstacleGrid[m-1][n-1] = -1
 	} else {
 		return 0
 	}
@@ -18,39 +14,25 @@ func uniquePathsWithObstacles(obstacleGrid [][]int) int {
 		for p := 0; p < m; p++ {
 			q := i - p
 			if q >= 0 && q < n {
-				//fmt.Print("[", p, q, "] ")
 				cur := obstacleGrid[p][q]
 				if cur == 1 {
-					//无事发生
+					obstacleGrid[0][0] = 0
 				} else {
-					if p == 0 {
-						if q == 0 {
-							return table[p][q]
-						} else {
-							if obstacleGrid[p][q-1] != 1 {
-								table[p][q-1] += table[p][q]
-							}
-						}
-					} else {
-						if q == 0 {
-							if obstacleGrid[p-1][q] != 1 {
-								table[p-1][q] += table[p][q]
-							}
-						} else {
-							if obstacleGrid[p][q-1] != 1 {
-								table[p][q-1] += table[p][q]
-							}
-							if obstacleGrid[p-1][q] != 1 {
-								table[p-1][q] += table[p][q]
-							}
+					if p != 0 {
+						if obstacleGrid[p-1][q] != 1 {
+							obstacleGrid[p-1][q] += cur
 						}
 					}
-
+					if q != 0 {
+						if obstacleGrid[p][q-1] != 1 {
+							obstacleGrid[p][q-1] += cur
+						}
+					}
 				}
 			}
 		}
 	}
-	return table[0][0]
+	return -obstacleGrid[0][0]
 }
 
 func main() {
