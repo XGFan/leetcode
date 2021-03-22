43958b5b6ace4d143ea85168ab394aaba5145ed0
Guofan Xu <xuguofan@live.com>
2021-03-22 17:20:23 +0800

    improve add binary

--- golang/AddBinary.go
+++ golang/AddBinary.go
@@ -7,72 +7,32 @@ func addBinary(a string, b string) string {
 	as := []byte(a)
 	bs := []byte(b)
 	t := 0
-	bytes := make([]byte, 0)
-	//bytes := make([]byte, len(b))
-	//copy(bytes, bs)
-	i2 := len(b) - len(a)
+	ft := len(b) - len(a)
 	for i := len(a) - 1; i >= 0; i-- {
-		if as[i] == '1' {
-			if bs[i2+i] == '1' {
-				if t == 0 {
-					bytes = append(bytes, '0')
-					t = 1
-				} else {
-					bytes = append(bytes, '1')
-					t = 1
-				}
-			} else {
-				if t == 0 {
-					bytes = append(bytes, '1')
-					t = 0
-				} else {
-					bytes = append(bytes, '0')
-					t = 1
-				}
-			}
+		bi := ft + i
+		if as[i] == '1' && bs[bi] == '1' {
+			bs[bi] = byte(48 + t)
+			t = 1
+		} else if as[i] == '0' && bs[bi] == '0' {
+			bs[bi] = byte(48 + t)
+			t = 0
 		} else {
-			if bs[i2+i] == '1' {
-				if t == 0 {
-					bytes = append(bytes, '1')
-					t = 0
-				} else {
-					bytes = append(bytes, '0')
-					t = 1
-				}
-			} else {
-				if t == 0 {
-					bytes = append(bytes, '0')
-					t = 0
-				} else {
-					bytes = append(bytes, '1')
-					t = 0
-				}
-			}
+			bs[bi] = byte(48 + t ^ 1)
 		}
 	}
 	if t == 0 {
-		for i := 0; i < len(bytes)/2; i++ {
-			bytes[i], bytes[len(bytes)-1-i] = bytes[len(bytes)-1-i], bytes[i]
-		}
-		return string(append(bs[:i2], bytes...))
+		return string(bs)
 	} else {
-		for i := i2 - 1; i >= 0; i-- {
+		for i := ft - 1; i >= 0; i-- {
 			if bs[i] == '1' {
-				bytes = append(bytes, '0')
+				bs[i] = '0'
 			} else {
-				bytes = append(bytes, '1')
-				for j := 0; j < len(bytes)/2; j++ {
-					bytes[j], bytes[len(bytes)-1-j] = bytes[len(bytes)-1-j], bytes[j]
-				}
-				return string(append(bs[:i], bytes...))
+				bs[i] = '1'
+				return string(bs)
 			}
 		}
 	}
-	bytes = append(bytes, '1')
-	for j := 0; j < len(bytes)/2; j++ {
-		bytes[j], bytes[len(bytes)-1-j] = bytes[len(bytes)-1-j], bytes[j]
-	}
-	return string(bytes)
+	return string(append([]byte{'1'}, bs...))
 }
 
 func exchange(a, b string) (string, string) {
