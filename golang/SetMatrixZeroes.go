22535bf63570fe422ef3be71a1cde3ce2805ebf3
Guofan Xu <xuguofan@live.com>
2021-03-24 14:52:27 +0800

    update Set Matrix Zeroes

--- golang/SetMatrixZeroes.go
+++ golang/SetMatrixZeroes.go
@@ -1,19 +1,23 @@
 package main
 
 func setZeroes(matrix [][]int) {
-	xs := make([]int, 0)
 	ys := make([]int, 0)
 	for x := range matrix {
+		justClean := false
 		for y := range matrix[x] {
 			if matrix[x][y] == 0 {
-				xs = append(xs, x)
 				ys = append(ys, y)
+				if !justClean {
+					for i := 0; i < y; i++ {
+						matrix[x][i] = 0
+					}
+					justClean = true
+				}
+			} else {
+				if justClean {
+					matrix[x][y] = 0
+				}
 			}
-		}
-	}
-	for _, x := range xs {
-		for y := range matrix[x] {
-			matrix[x][y] = 0
 		}
 	}
 	for _, y := range ys {
