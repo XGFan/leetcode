6b668916bcd71685c523d27c7cca9aac99f28634
Guofan Xu <xuguofan@live.com>
2021-03-27 12:37:33 +0800

    tricky

--- golang/Subsets.go
+++ golang/Subsets.go
@@ -5,8 +5,7 @@ import "fmt"
 func g(res *[][]int, prefix, other []int) {
 	for i := range other {
 		ints := make([]int, len(prefix)+1)
-		copy(ints, prefix)
-		ints[len(ints)-1] = other[i]
+		ints[copy(ints, prefix)] = other[i]
 		*res = append(*res, ints)
 		g(res, ints, other[i+1:])
 	}
