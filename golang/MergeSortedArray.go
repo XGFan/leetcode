1ea7d28861fa02128258705065bfa802d1c744c9
Guofan Xu <xuguofan@live.com>
2021-03-31 20:31:21 +0800

    update

--- golang/MergeSortedArray.go
+++ golang/MergeSortedArray.go
@@ -3,12 +3,9 @@ package main
 func merge_(nums1 []int, m int, nums2 []int, n int) {
 	nums1 = nums1[:m+n]
 	i, j := 0, 0
-	for {
+	for j < n {
 		if i >= m {
-			copy(nums1[len(nums1)-n+j:], nums2[j:n])
-			return
-		}
-		if j >= n {
+			copy(nums1[i:], nums2[j:n])
 			return
 		}
 		if nums1[i] > nums2[j] {
