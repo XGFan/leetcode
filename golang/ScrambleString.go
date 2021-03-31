b0e110d4c11d6c1136b959ef3693700b218377f8
Guofan Xu <xuguofan@live.com>
2021-03-31 17:09:19 +0800

    improve

--- golang/ScrambleString.go
+++ golang/ScrambleString.go
@@ -6,33 +6,56 @@ func isScramble(s1 string, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
-	return mayEqual(s1, s2)
+	return mayEqual([]byte(s1), []byte(s2))
 }
 
-func mayEqual(s1, s2 string) bool {
-	if s1 == s2 {
-		return true
-	}
-	t := make([]int, 128)
-	for _, v := range s1 {
-		t[v]++
-	}
-	for _, v := range s2 {
-		t[v]--
-		if t[v] < 0 {
-			return false
-		}
-	}
-	for s1[0] == s2[0] {
+func mayEqual(s1, s2 []byte) bool {
+	for len(s1) != 0 && s1[0] == s2[0] {
 		s1 = s1[1:]
 		s2 = s2[1:]
 	}
+	if len(s1) == 0 {
+		return true
+	}
+	left := make([]int, 128)
+	right := make([]int, 128)
+	lc, rc := 0, 0
 	for i := 1; i < len(s1); i++ {
-		if mayEqual(s1[0:i], s2[0:i]) && mayEqual(s1[i:], s2[i:]) {
-			return true
+		if left[s1[i-1]] == 0 {
+			lc++
+		} else if left[s1[i-1]] == -1 {
+			lc--
 		}
-		if mayEqual(s1[0:i], s2[len(s2)-i:]) && mayEqual(s1[i:], s2[0:len(s2)-i]) {
-			return true
+		if right[s1[i-1]] == 0 {
+			rc++
+		} else if right[s1[i-1]] == -1 {
+			rc--
+		}
+		left[s1[i-1]]++
+		right[s1[i-1]]++
+
+		if left[s2[i-1]] == 0 {
+			lc++
+		} else if left[s2[i-1]] == 1 {
+			lc--
+		}
+		if right[s2[len(s2)-i]] == 0 {
+			rc++
+		} else if right[s2[len(s2)-i]] == 1 {
+			rc--
+		}
+		left[s2[i-1]]--
+		right[s2[len(s2)-i]]--
+
+		if lc == 0 {
+			if mayEqual(s1[0:i], s2[0:i]) && mayEqual(s1[i:], s2[i:]) {
+				return true
+			}
+		}
+		if rc == 0 {
+			if mayEqual(s1[0:i], s2[len(s2)-i:]) && mayEqual(s1[i:], s2[0:len(s2)-i]) {
+				return true
+			}
 		}
 	}
 	return false
