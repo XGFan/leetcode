4d6207b5974c36f87cae60bbd0daffe8ec7a615c
Guofan <xuguofan@live.com>
2021-04-01 01:44:49 +0800

    improve Decode Ways

--- golang/DecodeWays.go
+++ golang/DecodeWays.go
@@ -3,29 +3,30 @@ package main
 import "fmt"
 
 func numDecodings(s string) int {
-	cache := make([]int, len(s)+1)
-	cache[len(s)] = 1
+	cache := make([]int, 2)
 	if s[len(s)-1] != '0' {
-		cache[len(s)-1] = 1
+		cache[0], cache[1] = 1, 1
+	} else {
+		cache[0], cache[1] = 0, 1
 	}
 	for i := len(s) - 2; i >= 0; i-- {
 		switch s[i] {
 		case '0':
-			cache[i] = 0
+			cache[0], cache[1] = 0, cache[0]
 		case '1':
 			if i < len(s)-1 {
-				cache[i] = cache[i+1] + cache[i+2]
+				cache[0], cache[1] = cache[0]+cache[1], cache[0]
 			} else {
-				cache[i] = cache[i+1]
+				cache[1] = cache[0]
 			}
 		case '2':
 			if i < len(s)-1 && s[i+1] <= '6' {
-				cache[i] = cache[i+1] + cache[i+2]
+				cache[0], cache[1] = cache[0]+cache[1], cache[0]
 			} else {
-				cache[i] = cache[i+1]
+				cache[1] = cache[0]
 			}
 		default:
-			cache[i] = cache[i+1]
+			cache[1] = cache[0]
 		}
 	}
 	return cache[0]
