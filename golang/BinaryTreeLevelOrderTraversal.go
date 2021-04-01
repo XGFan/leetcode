4780de5869917a95834859694dd05a7c822a21ea
Guofan <xuguofan@live.com>
2021-04-01 23:54:25 +0800

    update

--- golang/BinaryTreeLevelOrderTraversal.go
+++ golang/BinaryTreeLevelOrderTraversal.go
@@ -3,28 +3,29 @@ package main
 func levelOrder(root *TreeNode) [][]int {
 	res := make([][]int, 0)
 	if root != nil {
-		u2d(&res, []*TreeNode{root})
+		u2d(&res, &[]*TreeNode{root})
 	}
 	return res
 }
-func u2d(res *[][]int, nodes []*TreeNode) {
-	size := len(nodes)
+func u2d(res *[][]int, nodes *[]*TreeNode) {
+	size := len(*nodes)
 	if size == 0 {
 		return
 	}
 	ints := make([]int, 0, size)
 	for i := 0; i < size; i++ {
-		v := nodes[i]
+		v := (*nodes)[i]
 		ints = append(ints, v.Val)
 		if v.Left != nil {
-			nodes = append(nodes, v.Left)
+			*nodes = append(*nodes, v.Left)
 		}
 		if v.Right != nil {
-			nodes = append(nodes, v.Right)
+			*nodes = append(*nodes, v.Right)
 		}
 	}
 	*res = append(*res, ints)
-	u2d(res, nodes[:copy(nodes, nodes[size:])])
+	*nodes = (*nodes)[:copy(*nodes, (*nodes)[size:])]
+	u2d(res, nodes)
 }
 
 func main() {
