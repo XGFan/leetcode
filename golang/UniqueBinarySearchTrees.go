57c535f5aff42c831b2f0accebf9c064afd0cf69
Guofan Xu <xuguofan@live.com>
2021-04-01 12:05:11 +0800

    add cache

--- golang/UniqueBinarySearchTrees.go
+++ golang/UniqueBinarySearchTrees.go
@@ -3,22 +3,24 @@ package main
 import "fmt"
 
 func numTrees(n int) int {
-	return numTreesIter(n)
+	cache := make([]int, n+1)
+	cache[0] = 1
+	return numTreesIter(cache, n)
 }
 
-func numTreesIter(total int) int {
-	if total == 0 {
-		return 1
+func numTreesIter(cache []int, total int) int {
+	if cache[total] != 0 {
+		return cache[total]
 	}
 	res := 0
 	for i := 0; i <= total-1; i++ {
-		ltrees := numTreesIter(i)
-		rtrees := numTreesIter(total - 1 - i)
+		ltrees := numTreesIter(cache, i)
+		rtrees := numTreesIter(cache, total-1-i)
 		res += ltrees * rtrees
 	}
+	cache[total] = res
 	return res
 }
-
 func main() {
 	fmt.Println(numTrees(1))
 	fmt.Println(numTrees(2))
